f1bdd3fc0d004827419c95153945f5aeb731ba72
IPablo271 <69815580+IPablo271@users.noreply.github.com>
2022-10-18 10:33:39 -0600

    Cambios

--- RT2.py
+++ RT2.py
@@ -10,6 +10,7 @@ darkerBrown = Material(diffuse=color(178, 95, 3), albedo=[0.9,0.1],spec=0)
 black = Material(diffuse=color(0,0,0), albedo=[0.9,0.1], spec=40)
 red = Material(diffuse=color(255,50,52), albedo=[0.7,0.3], spec=60)
 metal = Material(diffuse=color(240,240,240),albedo=[0.9,0.1],spec=60)
+white2 = Material(diffuse=color(255,255,255),albedo =[1,0], spec=0)
 
 ray = Raytracer(1200, 1000)
 ray.light = Light(
@@ -24,26 +25,38 @@ ray.scene = [
 
     #Cabeza
     Sphere(V3(-3.0, -1.5, -10), 1.4, lighBrown),
+    Sphere(V3(3.0, -1.5, -10), 1.4, white2),
 
     #Ojos
     Sphere(V3(-3.1, -1.8, -8.8), 0.22, black),
     Sphere(V3(-2.3, -1.8, -9.0), 0.22, black),
+    Sphere(V3(3.1, -1.8, -8.8), 0.22, black),
+    Sphere(V3(2.3, -1.8, -9.0), 0.22, black),
 
     #Nariz
     Sphere(V3(-2.73, -1.4, -9.0), 0.48, darkerBrown),
+    Sphere(V3(-2.60, -1.4, -8.5), 0.20, black),
+    Sphere(V3(2.73, -1.4, -9.0), 0.48, metal),
+    Sphere(V3(2.60, -1.4, -8.5), 0.20, black),
 
 
     #Orejas
     Sphere(V3(-3.6, -2.5, -9), 0.6, darkerBrown),
     Sphere(V3(-1.6, -2.5, -10), 0.65, darkerBrown),
+    Sphere(V3(3.6, -2.5, -9), 0.6, white2),
+    Sphere(V3(1.6, -2.5, -10), 0.65, white2),
 
     #Brazos
     Sphere(V3(-4.4, -0.4, -9), 0.7, darkerBrown),
     Sphere(V3(-1.1, -0.4, -10), 0.7, darkerBrown),
+    Sphere(V3(4.4, -0.4, -9), 0.7, white2),
+    Sphere(V3(1.1, -0.4, -10), 0.7, white2),
 
     #Piernas
     Sphere(V3(-3.8, 1.4, -9), 0.7, darkerBrown),
     Sphere(V3(-1.4, 1.4, -10), 0.75, darkerBrown),
+     Sphere(V3(3.8, 1.4, -9), 0.7, white2),
+    Sphere(V3(1.4, 1.4, -10), 0.75, white2),
    
 
 
